13a79e00176622dbee6b579710b59a686250d09b
Ku <130153466+AngelaKu123@users.noreply.github.com>
2024-12-05 04:10:32 +0800

    Add files via upload

--- Assembly Language/hw7/hw7.s
+++ Assembly Language/hw7/hw7.s
@@ -11,13 +11,11 @@
 	.data
 	.align 4
 
-/* --- file name --- */
-.input_filename:
-    .ascii "input.txt\0"
-.output_filename:
+/* --- files --- */
+.input_filename_addr:
+	.ascii "input.txt\0"
+.output_filename_addr:
     .ascii "output.txt\0"
-.read_buffer:
-    .space 4  /* read 1 word one time */
 
 /* ========================= */
 /*       TEXT section        */
@@ -25,98 +23,124 @@
 	.section .text
 	.global main
 	.type main,%function
-
-.open_input_param:
-    .word .input_filename
-    .word 0x2
-    .word 0x9
+.input_filename:
+	.word .input_filename_addr
+.output_filename:
+    .word .output_filename_addr
+.open_param:
+	.word .input_filename_addr
+	.word 0x2
+	.word 0x9
 .open_output_param:
-    .word .output_filename
+    .word .output_filename_addr
     .word 0x4
     .word 0xA
 .flen_param:
 	.space 4
 .read_param:
-    .space 4
-    .word .read_buffer
-    .space 4
+	.space 4
+	.space 4
+	.space 4
 .write_param:
-    .space 4
-    .space 4
-    .space 4
-
+	.space 4   /* file descriptor */
+	.space 4  /* address of the string */
+	.space 4   /* length of the string */
+.close_param:
+	.space 4
 main:
-    mov ip, sp
+	mov ip, sp
 	push {fp, ip, lr, pc}
 	sub fp, ip, #4
 
-    /* open files */
-    MOV r0, #SWI_Open
-    ADR r1, .open_input_param
-    SWI AngelSWI
-    MOV r2, r0
+    /* open a file */
+	mov r0, #SWI_Open
+	adr r1, .open_param
+	swi AngelSWI
+	mov r2, r0                /* r2 is file descriptor */
 
     MOV r0, #SWI_Open
     ADR r1, .open_output_param
     SWI AngelSWI
-    MOV r3, r0
+    MOV r7, r0  /* r7 is output_filename */
 
-    /* get length of input.txt */
-    MOV r0, #SWI_FLen
-    ADR r1, .flen_param
-    STR r2, [r1, #0]
-    SWI AngelSWI
-    MOV r4, r0
+	/* get length of a file */
+	mov r0, #SWI_FLen
+	adr r1, .flen_param
+	str r2, [r1, #0]
+	swi AngelSWI
+	mov r6, r0
 
-    /* read from inpui.txt */
-    ADR r1, .read_param
-    STR r2, [r1, #0]
-    LDR r5, [r1, #4]
-    MOV r6, #1
-    STR r6, [r1, #8]
+loop:      
+    /* read from a file */
+	adr r1, .read_param
+	MOV r8, #0
+	STR r8, [r1, #0]
+	STR r8, [r1, #4]
+	STR r8, [r1, #8]
 
-read_loop:
-    CMP r4, #1
+	str r2, [r1, #0]          /* store file descriptor */
 
-    MOV r0, #SWI_Read
-    SWI AngelSWI
-    LDRB r7, [r5, #0]  /* r7 is the read data */
+	ldr r5, [r1, #4]          /* r5 is the address of readbuffer */
 
-    CMP r7, #'a'  /* if the word < a, 97 */
-    BLT skip_convert
-    CMP r7, #'z'  /* if the word > z, 122 */
-    BGT skip_convert
-    SUB r7, r7, #32  /* a - A(65) = 32 */
-    B write_step
+	mov r3, #1
+	str r3, [r1, #8]          /* store the length of the string */
 
-skip_convert:
-    B write_step
+	cmp r6, #0
+    BEQ add_end
+
+	mov r0, #SWI_Read
+	swi AngelSWI
+
+	ldrb r4, [r5, #0]         /* r4 is the read data */
+
+CMP r4, #'a'
+    BLT write_step
+    CMP r4, #'z'
+    BGT write_step
+    SUB r4, r4, #32  /* 97 - 65 */
 
 write_step:
-    MOV r0, #SWI_Write
     ADR r1, .write_param
-    STR r3, [r1, #0]
 
-    STR r7, [r1, #4]  /* r7 task done */
+    MOV r9, #0
+    STR r9, [r1, #0]
+    STR r9, [r1, #4]
+    STR r9, [r1, #8]
 
-    MOV r7, #4  /* new r7 */
-    STR r7, [r1, #8]
+    STR r7, [r1, #0]
+    STR r4, [r1, #4]
+    MOV r8, #1
+    STR r8, [r1, #8]
 
+    MOV r0, #SWI_Write
     SWI AngelSWI
 
-    SUB r4, r4, #1
-    BNE read_loop
-/* requirement done */
+	sub r6, r6, #1
+	bne loop
+
+add_end:
+    MOV r4, #'\n'
+    ADR r1, .write_param
+    STR r7, [r1, #0]
+    STR r4, [r1, #4]
+    STR r8, [r1, #8]
+
+    MOV r0, #SWI_Write
+    SWI AngelSWI
 
 close_files:
+	adr r1, .close_param
+	str r2, [r1, #0]
+
+	mov r0, #SWI_Close
+	swi AngelSWI
+
+    ADR r1, .close_param
+    STR r7, [r1, #0]
     MOV r0, #SWI_Close
-    MOV r1, r2
     SWI AngelSWI
 
-    MOV r1, r3
-    SWI AngelSWI
-
-    /* end of program */
     sub sp, fp, #12
     ldm sp, {fp, sp, lr}
     bx lr
+
