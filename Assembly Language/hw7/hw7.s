5f46e4ed8f6962bce91eaee6b07e72c01223b41c
Ku <130153466+AngelaKu123@users.noreply.github.com>
2024-12-05 17:16:09 +0800

    Add files via upload

--- Assembly Language/hw7/hw7.s
+++ Assembly Language/hw7/hw7.s
@@ -52,15 +52,15 @@
 .close_param:
 	.space 4
 main:
-	mov ip, sp
-	push {fp, ip, lr, pc}
-	sub fp, ip, #4
+	MOV ip, sp
+	PUSH {fp, ip, lr, pc}
+	SUB fp, ip, #4
 
     /* open a file */
-	mov r0, #SWI_Open
-	adr r1, .open_param
-	swi AngelSWI
-	mov r2, r0                /* r2 is file descriptor */
+	MOV r0, #SWI_Open
+	ADR r1, .open_param
+	SWI AngelSWI
+	MOV r2, r0                /* r2 is file descriptor */
 
     MOV r0, #SWI_Open
     ADR r1, .open_output_param
@@ -68,36 +68,33 @@ main:
     MOV r7, r0  /* r7 is output_filename */
 
 	/* get length of a file */
-	mov r0, #SWI_FLen
-	adr r1, .flen_param
-	str r2, [r1, #0]
-	swi AngelSWI
-	mov r6, r0
+	MOV r0, #SWI_FLen
+	ADR r1, .flen_param
+	STR r2, [r1, #0]
+	SWI AngelSWI
+	MOV r6, r0
 
 loop:      
-	cmp r6, #0
-    BEQ add_end
-
     /* read from a file */
-	adr r1, .read_param
+	ADR r1, .read_param
 	MOV r8, #0
 	STR r8, [r1, #0]
 	STR r8, [r1, #4]
 	STR r8, [r1, #8]
 
-	str r2, [r1, #0]          /* store file descriptor */
+	STR r2, [r1, #0]  /* r2: input file descriptor */
 
-	ldr r5, [r1, #4]          /* r5 is the address of readbuffer */
+	LDR r5, [r1, #4]  /* r5: address of read_buffer */
 
-	mov r3, #1
-	str r3, [r1, #8]          /* store the length of the string */
+	MOV r3, #1
+	STR r3, [r1, #8]  /* r3: length of read data */
 
-	mov r0, #SWI_Read
-	swi AngelSWI
+	MOV r0, #SWI_Read
+	SWI AngelSWI
 
-	ldrb r4, [r5, #0]         /* r4 is the read data */
+	LDRB r4, [r5, #0]  /* r4 is read data */
 
-CMP r4, #'a'
+	CMP r4, #'a'
     BLT write_step
     CMP r4, #'z'
     BGT write_step
@@ -112,39 +109,31 @@ write_step:
     STR r9, [r1, #8]
 
     STR r7, [r1, #0]
-    STRB r4, [r1, #4]
+    STRB r4, [r10]  /* r4 is data, r10 is new address point to r4 */
+	STR r10, [r1, #4]
     MOV r8, #1
     STR r8, [r1, #8]
 
     MOV r0, #SWI_Write
     SWI AngelSWI
 
-	sub r6, r6, #1
-	bne loop
-
-add_end:
-    MOV r4, #'\n'
-    ADR r1, .write_param
-    STR r7, [r1, #0]
-    STRB r4, [r1, #4]
-    STR r8, [r1, #8]
-
-    MOV r0, #SWI_Write
-    SWI AngelSWI
+	SUB r6, r6, #1
+	CMP r6, #0
+	BNE loop
 
 close_files:
-	adr r1, .close_param
-	str r2, [r1, #0]
+	ADR r1, .close_param
+	STR r2, [r1, #0]
 
-	mov r0, #SWI_Close
-	swi AngelSWI
+	MOV r0, #SWI_Close
+	SWI AngelSWI
 
     ADR r1, .close_param
     STR r7, [r1, #0]
     MOV r0, #SWI_Close
     SWI AngelSWI
 
-    sub sp, fp, #12
-    ldm sp, {fp, sp, lr}
-    bx lr
+    SUB sp, fp, #12
+    LDM sp, {fp, sp, lr}
+    BX lr
 
